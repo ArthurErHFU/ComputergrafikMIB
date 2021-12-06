8e0d03b2ef18de2a075bc5cfb6fd5711edda0813
ArthurErHFU <arthur.erlich@hs-furtwangen.de>
2021-12-06 18:38:38 +0100

    Add TUT9

--- Tut09_HierarchyAndInput/Tut09_HierarchyAndInput.cs
+++ Tut09_HierarchyAndInput/Tut09_HierarchyAndInput.cs
@@ -22,16 +22,54 @@ namespace FuseeApp
         private SceneRendererForward _sceneRenderer;
         private float _camAngle = 0;
         private Transform _baseTransform;
+        private Transform _bodyTransform;
 
+        private Transform _upperArmTransform;
+        private Transform _upperArmPivot;
 
-       SceneContainer CreateScene()
+        private Transform _lowerArmPivot;
+        private Transform _lowerArmTransform;
+
+        SceneContainer CreateScene()
         {
+            // Initialize nCuboid shape
+            float3 shape = new float3(2, 10, 2);
             // Initialize transform components that need to be changed inside "RenderAFrame"
             _baseTransform = new Transform
             {
                 Rotation = new float3(0, 0, 0),
                 Scale = new float3(1, 1, 1),
                 Translation = new float3(0, 0, 0)
+            };
+            _bodyTransform = new Transform
+            {
+                Rotation = new float3(0, 0, 0),
+                Scale = new float3(1, 1, 1),
+                Translation = new float3(0, 6, 0)
+            };
+            _upperArmPivot = new Transform
+            {
+                Rotation = new float3(0, 0, 0),
+                Scale = new float3(1, 1, 1),
+                Translation = new float3(2, 4, 0)
+            };
+            _upperArmTransform = new Transform
+            {
+                Rotation = new float3(0, 0, 0),
+                Scale = new float3(1, 1, 1),
+                Translation = new float3(0, 4, 0)
+            };
+            _lowerArmPivot = new Transform
+            {
+                Rotation = new float3(0, 0, 0),
+                Scale = new float3(1, 1, 1),
+                Translation = new float3(-2, 4, 0)
+            };
+            _lowerArmTransform = new Transform
+            {
+                Rotation = new float3(0, 0, 0),
+                Scale = new float3(1, 1, 1),
+                Translation = new float3(0, 4, 0)
             };
 
             // Setup the scene graph
@@ -52,7 +90,68 @@ namespace FuseeApp
                             // MESH COMPONENT
                             SimpleMeshes.CreateCuboid(new float3(10, 2, 10))
                         }
-                    }
+                    },
+                        //RED BODY
+                        new SceneNode
+                        {
+                            Components = new List<SceneComponent>
+                            {
+                                _bodyTransform,
+                                MakeEffect.FromDiffuseSpecular((float4) ColorUint.Red),
+                                SimpleMeshes.CreateCuboid(shape)
+                            },
+                            //GREEN BODY Pivot
+                            Children = new ChildList
+                            {
+                                new SceneNode
+                                {
+                                    Components = new List<SceneComponent>
+                                    {
+                                        _upperArmPivot
+                                    },
+                                    //GREEN BODY 
+                                    Children = new ChildList
+                                    {
+                                        new SceneNode
+                                        {
+                                            Components = new List<SceneComponent>
+                                            {
+                                                _upperArmTransform,
+                                                MakeEffect.FromDiffuseSpecular((float4) ColorUint.Green),
+                                                SimpleMeshes.CreateCuboid(shape)
+                                            },
+                                            //BLUE BODY Pivot
+                                            Children = new ChildList
+                                            {
+                                                new SceneNode
+                                                {
+                                                    Components = new List<SceneComponent>
+                                                    {
+                                                         _lowerArmPivot
+
+                                                    },
+                                                    Children = new ChildList
+                                                    {
+                                                        new SceneNode
+                                                        {
+                                                            Components= new List<SceneComponent>
+                                                            {
+                                                                _lowerArmTransform,
+                                                                MakeEffect.FromDiffuseSpecular((float4) ColorUint.Blue),
+                                                                SimpleMeshes.CreateCuboid(shape)
+                                                            }
+                                                        }
+                                                    }
+
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+
+                            }
+
+                        }
                 }
             };
         }
@@ -62,7 +161,8 @@ namespace FuseeApp
         public override void Init()
         {
             // Set the clear color for the backbuffer to white (100% intensity in all color channels R, G, B, A).
-            RC.ClearColor = new float4(0.8f, 0.9f, 0.7f, 1);
+            //RC.ClearColor = new float4(0.8f, 0.9f, 0.7f, 1);
+            RC.ClearColor = new float4((float3)ColorUint.WhiteSmoke, 1);
 
             _scene = CreateScene();
 
@@ -74,12 +174,19 @@ namespace FuseeApp
         public override void RenderAFrame()
         {
             SetProjectionAndViewport();
-            
+
             // Clear the backbuffer
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
 
             // Setup the camera 
             RC.View = float4x4.CreateTranslation(0, -10, 50) * float4x4.CreateRotationY(_camAngle);
+            //My Stuff  -->DeltaTime
+            //BSP:
+            float bodyRot = _bodyTransform.Rotation.y;
+            bodyRot += 1f * Keyboard.LeftRightAxis;
+            _upperArmPivot.Rotation = new float3(bodyRot, 0, 0);
+            _lowerArmPivot.Rotation = new float3(bodyRot, 0, 0);
+            //END_BSP
 
             // Render the scene on the current render context
             _sceneRenderer.Render(RC);
@@ -101,6 +208,6 @@ namespace FuseeApp
             // Back clipping happens at 2000 (Anything further away from the camera than 2000 world units gets clipped, polygons will be cut)
             var projection = float4x4.CreatePerspectiveFieldOfView(M.PiOver4, aspectRatio, 1, 20000);
             RC.Projection = projection;
-        }        
+        }
     }
 }
