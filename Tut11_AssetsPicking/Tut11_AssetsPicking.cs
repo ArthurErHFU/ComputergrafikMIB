dc8d9183d6c3599e334cf014a94351274296b543
ArthurErHFU <arthur.erlich@hs-furtwangen.de>
2022-01-14 17:56:17 +0100

    add pic and pic animation

--- Tut11_AssetsPicking/Tut11_AssetsPicking.cs
+++ Tut11_AssetsPicking/Tut11_AssetsPicking.cs
@@ -39,6 +39,13 @@ namespace FuseeApp
         private Transform _backRightWing;
         private Transform _backLeftWing;
 
+        //Weapon
+        private Transform _cannonBase;
+        private Transform _cannon;
+
+        //Picking
+        private PickResult _currentPick;
+        private float4 _oldColor;
 
         // Init is called on startup. 
         public override void Init()
@@ -57,13 +64,17 @@ namespace FuseeApp
             _midTireLeft = getTransform("MidTireLeft");
             _backRightTire = getTransform("BackRightTire");
             _backLeftTire = getTransform("BackLeftTire");
+
             //RotationPlatform
             _frontRightWing = getTransform("FrontRightWing");
             _frontLeftWing = getTransform("FrontLeftWing");
             _backRightWing = getTransform("BackRightWing");
             _backLeftWing = getTransform("BackLeftWing");
 
+            //Weapon
 
+            _cannonBase = getTransform("CannonBase");
+            _cannon = getTransform("Cannon");
             //scenepicker
             _scenePicker = new ScenePicker(_scene);
 
@@ -86,11 +97,7 @@ namespace FuseeApp
             SetProjectionAndViewport();
 
             //_mainPart.Rotation = new float3(0, M.MinAngle(TimeSinceStart / 10), 0);
-            driveForword(3);
-            turnLeftRight(1);
-            pickNode();
 
-            //TODO: add cannon controll
 
             // Clear the backbuffer
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
@@ -102,6 +109,13 @@ namespace FuseeApp
             _sceneRenderer.Render(RC);
 
             // Swap buffers: Show the contents of the backbuffer (containing the currently rendered frame) on the front buffer.
+
+            // PlyeerControll
+            driveForword(3);
+            turnLeftRight(1);
+            pickNode();
+            picAnimation(_currentPick);
+            weaponTrun(1.5f);
             Present();
         }
 
@@ -123,6 +137,7 @@ namespace FuseeApp
         {
             return _scene.Children.FindNodes(node => node.Name == name)?.FirstOrDefault()?.GetTransform();
         }
+
         private void pickNode()
         {
             if (Mouse.LeftButton)
@@ -130,16 +145,60 @@ namespace FuseeApp
                 float2 clipPos = Mouse.Position * new float2(2.0f / Width, -2.0f / Height) + new float2(-1.0f, 1.0f);
                 //works not well wiht my rover
                 PickResult newPick = _scenePicker.Pick(RC, clipPos).OrderBy(pr => pr.ClipPos.z).FirstOrDefault();
-                if (newPick != null)
+                if (newPick?.Node != _currentPick?.Node)
                 {
-                    Diagnostics.Debug(newPick.Node.Name);
+                    if (_currentPick != null)
+                    {
+                        //var ef = _currentPick.Node.GetComponent<DefaultSurfaceEffect>();
+                        //ef.SurfaceEffect.Albedo = _oldColor;
+                    }
+
+                    if (newPick != null)
+                    {
+                        var ef = newPick.Node.GetComponent<SurfaceEffect>();
+                        _oldColor = ef.SurfaceInput.Albedo;
+                        ef.SurfaceInput.Albedo = (float4)ColorUint.Greenery;
+                        _currentPick = newPick;
+                    }
+                }
+
+            }
+
+        }
+
+        private void picAnimation(PickResult pick)
+        {
+            if (pick != null)
+            {
+                var trans = pick.Node.Parent.GetComponent<Transform>();
+                float time = M.MinAngle(TimeSinceStart);
+                float speed = 3f;
+
+                if (trans != null)
+                {
+                    if (Keyboard.GetKey(KeyCodes.Up))
+                    {
+                        trans.Rotation = new float3((speed * 10 + trans.Rotation.x), trans.Rotation.y, 0);
+                    }
+                    else if (Keyboard.GetKey(KeyCodes.Down))
+                    {
+                        trans.Rotation = new float3((-speed * 10 + trans.Rotation.x), trans.Rotation.y, 0);
+                    }
+                    else if (Keyboard.GetKey(KeyCodes.Left))
+                    {
+                        trans.Rotation = new float3(trans.Rotation.x, (speed + trans.Rotation.y), 0);
+                    }
+                    else if (Keyboard.GetKey(KeyCodes.Right))
+                    {
+                        trans.Rotation = new float3(trans.Rotation.x, (-speed + trans.Rotation.y), 0);
+                    }
                 }
             }
         }
-        private void getColor(string name)
-        {
 
-        }
+
+        // animation
+
         private void driveForword(float speed)
         {
             if (Keyboard.GetKey(KeyCodes.W))
@@ -185,7 +244,34 @@ namespace FuseeApp
                 _backLeftWing.Rotation = new float3(0, ((speed + _backLeftWing.Rotation.y) % M.TwoPi), 0);
             }
         }
-        private void rotationSpeed()
+
+        private void weaponTrun(float speed)
+        {
+            speed = speed / 100;
+            if (Keyboard.GetKey(KeyCodes.Q))
+            {
+                _cannonBase.Rotation = new float3(0, ((-speed + _cannonBase.Rotation.y) % M.TwoPi), 0);
+            }
+            else if (Keyboard.GetKey(KeyCodes.E))
+            {
+                _cannonBase.Rotation = new float3(0, ((speed + _cannonBase.Rotation.y) % M.TwoPi), 0);
+            }
+
+
+            if (Keyboard.GetKey(KeyCodes.F) && _cannon.Rotation.x <= M.DegreesToRadians(16))
+            {
+                _cannon.Rotation = new float3(((speed + _cannon.Rotation.x) % M.TwoPi), 0, 0);
+            }
+
+            else if (Keyboard.GetKey(KeyCodes.R) && _cannon.Rotation.x >= M.DegreesToRadians(-60))
+            {
+                _cannon.Rotation = new float3(((-speed + _cannon.Rotation.x) % M.TwoPi), 0, 0);
+            }
+
+        }
+
+        //calculate angel of the wehls and turn the Body
+        private void bodyRotationSpeed()
         {
 
         }
